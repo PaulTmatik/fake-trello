a099b103fd05a3a9eced243fb9f768a0430be465
Paul T.Matik <t.matik1@gmail.com>
2020-07-22 23:38:12 +0700

    Add button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import EditableText from "./components/EditableText";
+import Button from "./components/pure/Button";
 
 function App() {
   const [editTextValue, setEditTextValue] = useState("block 1");
   return (
-    <EditableText
-      value={editTextValue}
-      onChange={(e) => setEditTextValue(e.target.value)}
-    />
+    <>
+      <EditableText
+        value={editTextValue}
+        onChange={(e) => setEditTextValue(e.target.value)}
+      />
+      <Button icon="add">Добавить ещё одну колонку</Button>
+    </>
   );
 }
 
---
+++ src/components/pure/Button.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from "react";
+import Icon from "./Icon";
+
+import { combineClassNames } from "../../utils/classnameCombiner";
+
+import "./Button.css";
+
+export default class Button extends PureComponent {
+  render() {
+    const { children, icon, className } = this.props;
+    return (
+      <button className={combineClassNames("button", className)}>
+        {this.renderWithIcon(icon, children)}
+      </button>
+    );
+  }
+
+  renderWithIcon(iconName, content) {
+    return (
+      <>
+        {iconName && <Icon name={iconName} className="button__icon" />}
+        {content ? iconName ? <span>{content}</span> : content : null}
+      </>
+    );
+  }
+}
