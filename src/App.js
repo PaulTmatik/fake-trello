2835394690e02770b47c8f6db9a189d1dd0de24d
Paul T.Matik <t.matik1@gmail.com>
2020-07-24 10:46:18 +0700

    Add user form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 
-import store from './store';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import store from "./store";
 import Dashboard from "./components/Dashboard";
+import Register from "./components/pages/Register";
 
 function App() {
   return (
     <Provider store={store}>
-      <Dashboard />
+      <Router>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </Provider>
   );
 }
---
+++ src/components/pages/Register.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+import "./Register.css";
+import CustomInput from "../pure/CustomInput";
+import Button from "../pure/Button";
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_email: "",
+      user_password: "",
+      user_name: "",
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+  render() {
+    return (
+      <div className="register">
+        <header className="auth__head">
+          <h1 className="auth__header">Регистрация пользователя</h1>
+        </header>
+        <form className="auth__form">
+          <CustomInput
+            label="Имя"
+            type="text"
+            name="user_name"
+            id="register_name_input"
+            value={this.state.user_name}
+            required
+            onChange={this.onChangeHandler}
+          />
+
+          <CustomInput
+            label="Емайл"
+            type="email"
+            name="user_email"
+            id="register_email_input"
+            value={this.state.user_email}
+            required
+            onChange={this.onChangeHandler}
+          />
+
+          <CustomInput
+            label="Пароль"
+            type="password"
+            name="user_password"
+            id="register_password_input"
+            value={this.state.user_password}
+            required
+            onChange={this.onChangeHandler}
+          />
+
+          <Button className="button--column_area">Зарегистрироваться</Button>
+        </form>
+      </div>
+    );
+  }
+
+  onChangeHandler(e) {
+    const newState = { ...this.state };
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+}
+
+export default Register;
